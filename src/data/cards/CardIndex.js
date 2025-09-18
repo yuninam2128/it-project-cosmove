fb0ab642a618c01dfb95832ca099981dbd76ae44
isreal-6 <snowkitten867@gmail.com>
2025-09-18 13:48:52 +0900

    영감카드 완성 아이고 영감탱이

--- src/data/cards/CardIndex.js
+++ src/data/cards/CardIndex.js
@@ -8,26 +8,26 @@ import card4 from './inscard4.png';
 export const cardData = [
   {
     id: 1,
-    title: '기본 카드',
-    description: '기본 카드 설명',
+    title: '새로운 시도를!',
+    description: '평소 미뤄둔 다른 방식으로 접근해보세요. ',
     image: card1
   },
   {
     id: 2,
-    title: '특별 카드',
-    description: '특별 카드 설명', 
+    title: '작은 행성부터 정복하라',
+    description: '오늘은 세부 작업을 정리하기 좋은 날!', 
     image: card2
   },
   {
     id: 3,
-    title: '추가 카드',
-    description: '추가 카드 설명',
+    title: '우선순위를 바꿔보자!',
+    description: '가장 급한 일부터 처리해보자구~',
     image: card3
   },
   {
     id: 4,
-    title: '추가 카드',
-    description: '추가 카드 설명',
+    title: '집중 모드 On',
+    description: '한 가지 일에 몰입해 성취감을 느껴보세요. ',
     image: card4
   }
 ];
